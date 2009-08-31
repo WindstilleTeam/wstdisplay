9f326b230edc4a175cf4633f392c10f119cda784
Ingo Ruhnke <grumbel@gmail.com>
2009-08-31 19:40:36 +0000

    Framework for making the ParticleSystem usable in the SceneGraph

---
+++ src/scenegraph/particle_system_drawable.cpp
@@ -0,0 +1,35 @@
+/*
+**  Windstille - A Sci-Fi Action-Adventure Game
+**  Copyright (C) 2009 Ingo Ruhnke <grumbel@gmx.de>
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**  
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**  
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "scenegraph/particle_system_drawable.hpp"
+
+#include "particles/particle_system.hpp"
+
+ParticleSystemDrawable::ParticleSystemDrawable(const ParticleSystem& particle_system)
+  : Drawable(Vector2f()),
+    m_particle_system(particle_system)
+{
+}
+
+void
+ParticleSystemDrawable::draw(const Texture& /*tmp_texture*/)
+{
+  m_particle_system.draw();
+}
+
+/* EOF */
