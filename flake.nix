f93ae3e9abde3de6c9b6a787d077ea0cf5f89d74
Ingo Ruhnke <grumbel@gmail.com>
2022-02-27 16:07:26 +0100

    Remove superfluous nix input from Nix flake

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "Windstille Display Engine";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    nix.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "gitlab:grumbel/cmake-modules";
@@ -11,25 +10,21 @@
     tinycmmc.inputs.flake-utils.follows = "flake-utils";
 
     babyxml.url = "gitlab:grumbel/babyxml";
-    babyxml.inputs.nix.follows = "nix";
     babyxml.inputs.nixpkgs.follows = "nixpkgs";
     babyxml.inputs.flake-utils.follows = "flake-utils";
     babyxml.inputs.tinycmmc.follows = "tinycmmc";
 
     logmich.url = "gitlab:logmich/logmich";
-    logmich.inputs.nix.follows = "nix";
     logmich.inputs.nixpkgs.follows = "nixpkgs";
     logmich.inputs.flake-utils.follows = "flake-utils";
     logmich.inputs.tinycmmc.follows = "tinycmmc";
 
     geomcpp.url = "gitlab:grumbel/geomcpp";
-    geomcpp.inputs.nix.follows = "nix";
     geomcpp.inputs.nixpkgs.follows = "nixpkgs";
     geomcpp.inputs.flake-utils.follows = "flake-utils";
     geomcpp.inputs.tinycmmc.follows = "tinycmmc";
 
     surfcpp.url = "gitlab:grumbel/surfcpp";
-    surfcpp.inputs.nix.follows = "nix";
     surfcpp.inputs.nixpkgs.follows = "nixpkgs";
     surfcpp.inputs.flake-utils.follows = "flake-utils";
     surfcpp.inputs.tinycmmc.follows = "tinycmmc";
@@ -37,7 +32,7 @@
     surfcpp.inputs.logmich.follows = "logmich";
   };
 
-  outputs = { self, nix, nixpkgs, flake-utils, tinycmmc, babyxml, geomcpp, logmich, surfcpp }:
+  outputs = { self, nixpkgs, flake-utils, tinycmmc, babyxml, geomcpp, logmich, surfcpp }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -47,6 +42,7 @@
             pname = "wstdisplay";
             version = "0.3.0";
             src = nixpkgs.lib.cleanSource ./.;
+            cmakeFlags = [ "-DBUILD_EXTRA=ON" ];
             nativeBuildInputs = [
               pkgs.cmake
               pkgs.ninja
