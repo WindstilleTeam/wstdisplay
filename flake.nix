5f93ce24d7d5ef0cabefb373d98cb5368424c83c
Ingo Ruhnke <grumbel@gmail.com>
2022-08-20 17:48:01 +0200

    Add Windows cross compile no Nix flake

--- flake.nix
+++ flake.nix
@@ -3,78 +3,108 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
-    tinycmmc.inputs.flake-utils.follows = "flake-utils";
 
     babyxml.url = "github:grumbel/babyxml";
     babyxml.inputs.nixpkgs.follows = "nixpkgs";
-    babyxml.inputs.flake-utils.follows = "flake-utils";
     babyxml.inputs.tinycmmc.follows = "tinycmmc";
 
     logmich.url = "github:logmich/logmich";
     logmich.inputs.nixpkgs.follows = "nixpkgs";
-    logmich.inputs.flake-utils.follows = "flake-utils";
     logmich.inputs.tinycmmc.follows = "tinycmmc";
 
     geomcpp.url = "github:grumbel/geomcpp";
     geomcpp.inputs.nixpkgs.follows = "nixpkgs";
-    geomcpp.inputs.flake-utils.follows = "flake-utils";
     geomcpp.inputs.tinycmmc.follows = "tinycmmc";
+
+    SDL2-win32.url = "github:grumnix/SDL2-win32";
+    SDL2-win32.inputs.nixpkgs.follows = "nixpkgs";
+    SDL2-win32.inputs.tinycmmc.follows = "tinycmmc";
+
+    glew-win32.url = "github:grumnix/glew-win32";
+    glew-win32.inputs.nixpkgs.follows = "nixpkgs";
+    glew-win32.inputs.tinycmmc.follows = "tinycmmc";
+
+    freetype-win32.url = "github:grumnix/freetype-win32";
+    freetype-win32.inputs.nixpkgs.follows = "nixpkgs";
+    freetype-win32.inputs.tinycmmc.follows = "tinycmmc";
 
     surfcpp.url = "github:grumbel/surfcpp";
     surfcpp.inputs.nixpkgs.follows = "nixpkgs";
-    surfcpp.inputs.flake-utils.follows = "flake-utils";
     surfcpp.inputs.tinycmmc.follows = "tinycmmc";
     surfcpp.inputs.geomcpp.follows = "geomcpp";
     surfcpp.inputs.logmich.follows = "logmich";
+    surfcpp.inputs.SDL2-win32.follows = "SDL2-win32";
   };
 
-  outputs = { self, nixpkgs, flake-utils, tinycmmc, babyxml, geomcpp, logmich, surfcpp }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = flake-utils.lib.flattenTree {
+  outputs = { self, nixpkgs, tinycmmc, babyxml, geomcpp, logmich, surfcpp, freetype-win32, SDL2-win32, glew-win32 }:
+    tinycmmc.lib.eachSystemWithPkgs (pkgs:
+      {
+        packages = rec {
+          default = wstdisplay;
+
           wstdisplay = pkgs.stdenv.mkDerivation {
             pname = "wstdisplay";
             version = "0.3.0";
+
             src = nixpkgs.lib.cleanSource ./.;
+
             cmakeFlags = [ "-DBUILD_EXTRA=ON" ];
+
+            postFixup =
+              (nixpkgs.lib.optionalString pkgs.targetPlatform.isWindows ''
+                mkdir -p $out/bin/
+                find ${pkgs.windows.mcfgthreads} -iname "*.dll" -exec ln -sfv {} $out/bin/ \;
+                find ${pkgs.stdenv.cc.cc} -iname "*.dll" -exec ln -sfv {} $out/bin/ \;
+                ln -sfv ${SDL2-win32.packages.${pkgs.system}.default}/bin/*.dll $out/bin/
+                ln -sfv ${pkgs.fmt_8}/bin/*.dll $out/bin/
+                ln -sfv ${pkgs.gtest}/bin/*.dll $out/bin/
+                ln -sfv ${pkgs.libsigcxx}/bin/*.dll $out/bin/
+                ln -sfv ${freetype-win32.packages.${pkgs.system}.default}/bin/*.dll $out/bin/
+                ln -sfv ${glew-win32.packages.${pkgs.system}.default}/bin/*.dll $out/bin/
+
+                # FIXME: These should be handled in surfcpp or statically linked
+                ln -sfv ${pkgs.libjpeg_original.overrideAttrs (oldAttrs: { meta = {}; })}/bin/*.dll $out/bin/
+                ln -sfv ${pkgs.libpng.overrideAttrs (oldAttrs: { meta = {}; })}/bin/*.dll $out/bin/
+                ln -sfv ${pkgs.zlib.overrideAttrs (oldAttrs: { meta = {}; })}/bin/*.dll $out/bin/
+            '');
+
             nativeBuildInputs = [
-              pkgs.cmake
-              pkgs.ninja
-              pkgs.gcc
-              pkgs.pkgconfig
+              pkgs.buildPackages.cmake
+              pkgs.buildPackages.pkgconfig
             ];
+
             buildInputs = [
-              tinycmmc.defaultPackage.${system}
+              tinycmmc.packages.${pkgs.system}.default
 
               pkgs.gtest
             ];
-            propagatedBuildInputs = [
-              babyxml.defaultPackage.${system}
-              geomcpp.defaultPackage.${system}
-              logmich.defaultPackage.${system}
-              surfcpp.defaultPackage.${system}
 
-              pkgs.freetype
-              pkgs.libGL
-              pkgs.libGLU
-              pkgs.glew
-              pkgs.fmt
-              pkgs.glm
-              pkgs.SDL2
-              pkgs.libjpeg
-              pkgs.libpng
+            propagatedBuildInputs = [
+              babyxml.packages.${pkgs.system}.default
+              geomcpp.packages.${pkgs.system}.default
+              logmich.packages.${pkgs.system}.default
+              surfcpp.packages.${pkgs.system}.default
+
               pkgs.libsigcxx
-              pkgs.imagemagick6
-              pkgs.libexif
+              pkgs.fmt_8
+
+              (if pkgs.targetPlatform.isWindows
+               then freetype-win32.packages.${pkgs.system}.default
+               else pkgs.freetype)
+
+              (if pkgs.targetPlatform.isWindows
+               then glew-win32.packages.${pkgs.system}.default
+               else pkgs.glew)
+
+              (if pkgs.targetPlatform.isWindows
+               then SDL2-win32.packages.${pkgs.system}.default
+               else pkgs.SDL2)
             ];
-           };
+          };
         };
-        defaultPackage = packages.wstdisplay;
-      });
+      }
+    );
 }
